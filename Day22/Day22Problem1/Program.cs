2f2597fcc2094ce4515e314b8fa72319e220ea4f
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2023-12-22 07:08:30 -0600

    Stub out day 22 projects

---
+++ Day22/Day22Problem1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day22Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Day22/Day22Problem2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day22Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
