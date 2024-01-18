8164d0b77d36068e21c97639d1d1502808bfc714
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-01-18 11:00:14 -0600

    Refactor Day 4 Problem 1 (#13)

--- Day04/Day04Problem1/Solver.cs
+++ Day04/Day04Problem1/Solver.cs
@@ -4,44 +4,13 @@
     {
         public long GetSolution(string[] inputLines)
         {
-            long cardScoreSum = 0;
+            var cards = new List<Card>();
             foreach (string cardLine in inputLines)
             {
-                cardScoreSum += GetCardScore(cardLine);
+                cards.Add(Card.Parse(cardLine));
             }
 
-            return cardScoreSum;
-        }
-
-        private static long GetCardScore(string cardLine)
-        {
-            string justNumbers = cardLine.Split(':')[1];
-            string[] twoHalves = justNumbers.Split('|');
-            string allWinnerString = twoHalves[0];
-            var winners = new HashSet<int>();
-            foreach (string winnerString in allWinnerString.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-            {
-                winners.Add(int.Parse(winnerString));
-            }
-
-            string allCardNumberString = twoHalves[1];
-            var cardNumbers = new List<int>();
-            foreach (string cardNumberString in allCardNumberString.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-            {
-                cardNumbers.Add(int.Parse(cardNumberString));
-            }
-
-            long score = 0;
-            foreach (int number in cardNumbers)
-            {
-                if (winners.Contains(number))
-                {
-                    if (score == 0) score = 1;
-                    else score = score * 2;
-                }
-            }
-
-            return score;
+            return cards.Sum(c => c.GetCardScore());
         }
     }
 }
