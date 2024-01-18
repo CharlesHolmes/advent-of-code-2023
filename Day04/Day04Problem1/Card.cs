1e60a1a3c9d77ace1e5d165fbed5f79115945349
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-01-18 10:58:57 -0600

    Refactor Day 4 Problem 1

---
+++ Day04/Day04Problem1/Card.cs
@@ -0,0 +1,32 @@
+﻿namespace Day04Problem1
+{
+    public class Card
+    {
+        private readonly HashSet<int> _drawnNumbers = new();
+        private readonly HashSet<int> _winningNumbers = new();
+
+        private Card() { }
+
+        public static Card Parse(string cardLine)
+        {
+            var card = new Card();
+            string justNumbers = cardLine.Split(':')[1];
+            string[] twoHalves = justNumbers.Split('|');
+            string winningNumberString = twoHalves[0];
+            card._winningNumbers.UnionWith(ParseNumbers(winningNumberString));
+            string drawnNumberString = twoHalves[1];
+            card._drawnNumbers.UnionWith(ParseNumbers(drawnNumberString));
+            return card;
+        }
+
+        private static IEnumerable<int> ParseNumbers(string input) =>
+            input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s));
+
+        public long GetCardScore()
+        {
+            int matchCount = _drawnNumbers.Intersect(_winningNumbers).Count();
+            if (matchCount == 0) return 0;
+            else return (long)Math.Pow(2, matchCount - 1);
+        }
+    }
+}
