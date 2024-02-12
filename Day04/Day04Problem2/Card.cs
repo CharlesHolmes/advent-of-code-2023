ba0d819b4377546dccdabe0930dc629e46c5a460
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-12 09:09:15 -0600

    Unit test day 4 problem 2

---
+++ Day04/Day04Problem2/Card.cs
@@ -0,0 +1,44 @@
+﻿namespace Day04Problem2
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public HashSet<int> WinningNumbers { get; } = new HashSet<int>();
+        public List<int> CardNumbers { get; } = new List<int>();
+        public int WinnerCount { get; set; }
+
+        public static Card ParseCard(string cardLine)
+        {
+            var card = new Card();
+            string[] idAndNumbers = cardLine.Split(':');
+            string idHalf = idAndNumbers[0];
+            string cardIdString = idHalf.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+            card.Id = int.Parse(cardIdString);
+            string justNumbers = idAndNumbers[1];
+            string[] numberHalves = justNumbers.Split('|');
+            string allWinnerString = numberHalves[0];
+            foreach (string winnerString in allWinnerString.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            {
+                card.WinningNumbers.Add(int.Parse(winnerString));
+            }
+
+            string allCardNumberString = numberHalves[1];
+            foreach (string cardNumberString in allCardNumberString.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            {
+                card.CardNumbers.Add(int.Parse(cardNumberString));
+            }
+
+            int score = 0;
+            foreach (int number in card.CardNumbers)
+            {
+                if (card.WinningNumbers.Contains(number))
+                {
+                    score++;
+                }
+            }
+
+            card.WinnerCount = score;
+            return card;
+        }
+    }
+}
---
+++ Day04/Day04Problem2/Solver.cs
@@ -0,0 +1,34 @@
+﻿namespace Day04Problem2
+{
+    public class Solver
+    {
+        public long GetSolution(string[] inputLines)
+        {
+            var cardDictionary = new Dictionary<int, Card>();
+            foreach (string cardLine in inputLines)
+            {
+                var card = Card.ParseCard(cardLine);
+                cardDictionary.Add(card.Id, card);
+            }
+
+            var toProcess = new Queue<Card>();
+            foreach (Card card in cardDictionary.Values)
+            {
+                toProcess.Enqueue(card);
+            }
+
+            long totalCardCount = 0;
+            while (toProcess.Any())
+            {
+                var current = toProcess.Dequeue();
+                totalCardCount++;
+                for (int i = 1; i <= current.WinnerCount; i++)
+                {
+                    toProcess.Enqueue(cardDictionary[current.Id + i]);
+                }
+            }
+
+            return totalCardCount;
+        }
+    }
+}
