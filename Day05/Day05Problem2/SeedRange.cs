b6cf01285df0effa6f27a2c241db1380626d1c3e
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-16 14:47:47 -0600

    Refactor day 5 for time complexity

--- Day05/Day05Problem2/SeedRange.cs
+++ Day05/Day05Problem2/SeedRange.cs
@@ -1,11 +1,8 @@
 ﻿namespace Day05Problem2
 {
-    public partial class Solver
+    public class SeedRange
     {
-        public class SeedRange
-        {
-            public long Start { get; init; }
-            public long End { get; init; }
-        }
+        public long Start { get; init; }
+        public long End { get; init; }
     }
 }
