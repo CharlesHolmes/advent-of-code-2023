a026527be6899e752aa5f051d666e4bd07bffdcb
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-17 12:25:45 -0600

    Test day 6 problem 2 (#33)

---
+++ Day06/Day06Problem2/Solver.cs
@@ -0,0 +1,38 @@
+﻿namespace Day06Problem2
+{
+    public class Solver
+    {
+        public long GetSolution(string[] inputLines)
+        {
+            long time = long.Parse(string.Join("", inputLines[0].Split(':')[1].Where(char.IsDigit)));
+            long distanceRecord = long.Parse(string.Join("", inputLines[1].Split(':')[1].Where(char.IsDigit)));
+
+            long lowerWinningBound = 0;
+            for (long windupTime = 0; windupTime < time; windupTime++)
+            {
+                long raceTime = time - windupTime;
+                long distance = windupTime * raceTime;
+                if (distance > distanceRecord)
+                {
+                    lowerWinningBound = windupTime;
+                    break;
+                }
+            }
+
+            long upperWinningBound = 0;
+            for (long windupTime = time; windupTime >= 0; windupTime--)
+            {
+                long raceTime = time - windupTime;
+                long distance = windupTime * raceTime;
+                if (distance > distanceRecord)
+                {
+                    upperWinningBound = windupTime;
+                    break;
+                }
+            }
+
+            long waysToWin = upperWinningBound - lowerWinningBound + 1;
+            return waysToWin;
+        }
+    }
+}
