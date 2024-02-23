b2c8dc425952d721bdd1e8b268574c4ce3e9057a
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-23 10:18:13 -0600

    Test day 10

--- Day10/Day10Problem1/Program.cs
+++ Day10/Day10Problem1/Program.cs
@@ -1,111 +1,12 @@
 ﻿namespace Day10Problem1
 {
-    public struct Coord
+    internal static class Program
     {
-        public int Row;
-        public int Col;
-    }
-
-    internal class Program
-    {
-        private static Coord GetNextStep(char c, Coord current, Coord last)
-        {
-            var up = new Coord { Col = current.Col, Row = current.Row - 1 };
-            var down = new Coord { Col = current.Col, Row = current.Row + 1 };
-            var left = new Coord { Col = current.Col - 1, Row = current.Row };
-            var right = new Coord { Col = current.Col + 1, Row = current.Row };
-            switch (c)
-            {
-                case '|':
-                    if (up.Equals(last)) return down;
-                    else if (down.Equals(last)) return up;
-                    else throw new Exception("Should not have gotten here.");
-                case '-':
-                    if (left.Equals(last)) return right;
-                    else if (right.Equals(last)) return left;
-                    else throw new Exception("Should not have gotten here.");
-                case 'L':
-                    if (up.Equals(last)) return right;
-                    else if (right.Equals(last)) return up;
-                    else throw new Exception("Should not have gotten here.");
-                case 'J':
-                    if (up.Equals(last)) return left;
-                    else if (left.Equals(last)) return up;
-                    else throw new Exception("Should not have gotten here.");
-                case '7':
-                    if (left.Equals(last)) return down;
-                    else if (down.Equals(last)) return left;
-                    else throw new Exception("Should not have gotten here.");
-                case 'F':
-                    if (down.Equals(last)) return right;
-                    else if (right.Equals(last)) return down;
-                    else throw new Exception("Should not have gotten here.");
-            }
-
-            throw new Exception("unrecognized char");
-        }
-
         static async Task Main(string[] args)
         {
-            Coord startingTile = new Coord { Row = -1, Col = -1 };
             var inputLines = await File.ReadAllLinesAsync(args[0]);
-            for (int row = 0; row < inputLines.Length; row++)
-            {
-                for (int col = 0; col < inputLines[row].Length; col++)
-                {
-                    if (inputLines[row][col] == 'S') startingTile = new Coord
-                    {
-                        Row = row,
-                        Col = col
-                    };
-                }
-            }
-
-            Coord last = new Coord { Row = -1, Col = -1 };
-            Coord current = new Coord { Row = -1, Col = -1 };
-            if (startingTile.Row > 0)
-            {
-                char above = inputLines[startingTile.Row - 1][startingTile.Col];
-                char right = inputLines[startingTile.Row][startingTile.Col + 1];
-                char below = inputLines[startingTile.Row + 1][startingTile.Col];
-                char left = inputLines[startingTile.Row][startingTile.Col - 1];
-                if (above == '|' || above == '7' || above == 'F')
-                {
-                    current = new Coord { Row = startingTile.Row - 1, Col = startingTile.Col };
-                    last = startingTile;
-                }
-                else if (right == '-' || right == 'J' || right == '7')
-                {
-                    current = new Coord { Row = startingTile.Row, Col = startingTile.Col + 1 };
-                    last = startingTile;
-                }
-                else if (below == '|' || below == 'L' || below == 'J')
-                {
-                    current = new Coord { Row = startingTile.Row + 1, Col = startingTile.Col };
-                    last = startingTile;
-
-                }
-                else if (left == '-' || left == 'L' || left == 'F')
-                {
-                    current = new Coord { Row = startingTile.Row, Col = startingTile.Col - 1 };
-                    last = startingTile;
-                }
-                else
-                {
-                    throw new Exception("nope");
-                }
-            }
-
-            long steps = 1;
-            while (!current.Equals(startingTile))
-            {
-                Coord temp = current;
-                current = GetNextStep(inputLines[current.Row][current.Col], current, last);
-                last = temp;
-                steps++;
-            }
-
-            Console.Out.WriteLine(steps / 2);
+            var solver = new Solver();
+            Console.Out.WriteLine(solver.GetSolution(inputLines));
         }
     }
 }
