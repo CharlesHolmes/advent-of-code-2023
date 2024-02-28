2459296087b0fccff8708451ac0c2b8aba6ff2de
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-28 10:34:30 -0600

    Test day 15 (#57)
    * Test day 15 problem 1
    
    * Test day 15 problem 2

---
+++ Day15/Day15Problem2/LensStep.cs
@@ -0,0 +1,58 @@
+﻿namespace Day15Problem2
+{
+    public class LensStep
+    {
+        private const string INPUT_EXCEPTION = "Invalid lens step input string.";
+        private readonly string _inputString;
+
+        public string LensLabel { get; init; }
+        public int? LensFocalLength { get; init; }
+        public int LensBox { get; init; }
+        public LensStepAction Action { get; init; }
+
+        public LensStep(string inputString)
+        {
+            _inputString = inputString;
+            if (_inputString.Contains('-'))
+            {
+                Action = LensStepAction.Remove;
+                string[] parts = _inputString.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 1)
+                {
+                    throw new ArgumentException(INPUT_EXCEPTION, nameof(inputString));
+                }
+                LensLabel = parts[0];
+                LensBox = GetStepBox(LensLabel);
+            }
+            else if (_inputString.Contains('='))
+            {
+                Action = LensStepAction.Add;
+                string[] parts = _inputString.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 2)
+                {
+                    throw new ArgumentException(INPUT_EXCEPTION, nameof(inputString));
+                }
+                LensLabel = parts[0];
+                LensFocalLength = int.Parse(parts[1]);
+                LensBox = GetStepBox(LensLabel);
+            }
+            else
+            {
+                throw new ArgumentException(INPUT_EXCEPTION, nameof(inputString));
+            }
+        }
+
+        private static int GetStepBox(string label)
+        {
+            int stepValue = 0;
+            foreach (int charAsciiValue in label)
+            {
+                stepValue += charAsciiValue;
+                stepValue *= 17;
+                stepValue %= 256;
+            }
+
+            return stepValue;
+        }
+    }
+}
---
+++ Day15/Day15Problem2/Solver.cs
@@ -0,0 +1,98 @@
+﻿namespace Day15Problem2
+{
+    public class Solver
+    {
+        public long GetSolution(string[] inputLines)
+        {
+            if (inputLines.Length != 1)
+            {
+                throw new ArgumentException("Input text must have exactly one line.", nameof(inputLines));
+            }
+            string[] splitInput = inputLines[0].Split(',', StringSplitOptions.RemoveEmptyEntries);
+            List<LensStep>[] allBoxes = InitEmptyBoxes();
+            foreach (string inputPart in splitInput)
+            {
+                var lensStep = new LensStep(inputPart);
+                List<LensStep> box = allBoxes[lensStep.LensBox];
+                if (lensStep.Action == LensStepAction.Add)
+                {
+                    AddLensToBox(box, lensStep);
+                }
+                else if (lensStep.Action == LensStepAction.Remove)
+                {
+                    RemoveLensFromBox(box, lensStep);
+                }
+                else
+                {
+                    throw new ArgumentException("Unrecognized input step action.");
+                }
+            }
+
+            return GetAllBoxesPower(allBoxes);
+        }
+
+        private static List<LensStep>[] InitEmptyBoxes()
+        {
+            const int boxCount = 256;
+            var result = new List<LensStep>[boxCount];
+            for (int i = 0; i < boxCount; i++)
+            {
+                result[i] = new List<LensStep>();
+            }
+
+            return result;
+        }
+
+        private static void AddLensToBox(List<LensStep> box, LensStep newLens)
+        {
+            bool replacedExisting = false;
+            for (int i = 0; i < box.Count; i++)
+            {
+                if (box[i].LensLabel == newLens.LensLabel)
+                {
+                    box[i] = newLens;
+                    replacedExisting = true;
+                }
+            }
+
+            if (!replacedExisting)
+            {
+                box.Add(newLens);
+            }
+        }
+
+        private static void RemoveLensFromBox(List<LensStep> box, LensStep toRemove)
+        {
+            for (int i = 0; i < box.Count; i++)
+            {
+                if (box[i].LensLabel == toRemove.LensLabel)
+                {
+                    box.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        private static long GetAllBoxesPower(List<LensStep>[] allBoxes)
+        {
+            long allLensPower = 0;
+            for (int i = 0; i < allBoxes.Length; i++)
+            {
+                for (int j = 0; j < allBoxes[i].Count; j++)
+                {
+                    int? lensFocalLength = allBoxes[i][j].LensFocalLength;
+                    if (lensFocalLength.HasValue)
+                    {
+                        allLensPower += (i + 1) * (j + 1) * lensFocalLength.Value;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Lens focal length must have a value.");
+                    }
+                }
+            }
+
+            return allLensPower;
+        }
+    }
+}
