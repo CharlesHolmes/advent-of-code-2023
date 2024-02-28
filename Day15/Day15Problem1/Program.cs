0136f2678ec1ad00115bb89a1e64286c00e6ccbc
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-02-28 10:18:16 -0600

    Test day 15 problem 1

--- Day15/Day15Problem1/Program.cs
+++ Day15/Day15Problem1/Program.cs
@@ -1,29 +1,12 @@
-﻿using System.Diagnostics;
-
-namespace Day15Problem1
+﻿namespace Day15Problem1
 {
-    internal class Program
+    internal static class Program
     {
         static async Task Main(string[] args)
         {
             var inputLines = await File.ReadAllLinesAsync(args[0]);
-            Debug.Assert(inputLines.Length == 1);
-            string[] steps = inputLines[0].Split(',', StringSplitOptions.RemoveEmptyEntries);
-            long sumOfSteps = 0;
-            foreach (string step in steps)
-            {
-                int stepValue = 0;
-                foreach (int charAsciiValue in step)
-                {
-                    stepValue += charAsciiValue;
-                    stepValue *= 17;
-                    stepValue %= 256;
-                }
-
-                sumOfSteps += stepValue;
-            }
-
-            Console.Out.WriteLine(sumOfSteps);
+            var solver = new Solver();
+            Console.Out.WriteLine(solver.GetSolution(inputLines));
         }
     }
 }
---
+++ Day15/Day15Problem1/Solver.cs
@@ -0,0 +1,25 @@
+﻿namespace Day15Problem1
+{
+    public class Solver
+    {
+        public long GetSolution(string[] inputLines)
+        {
+            string[] steps = inputLines[0].Split(',', StringSplitOptions.RemoveEmptyEntries);
+            long sumOfSteps = 0;
+            foreach (string step in steps)
+            {
+                int stepValue = 0;
+                foreach (int charAsciiValue in step)
+                {
+                    stepValue += charAsciiValue;
+                    stepValue *= 17;
+                    stepValue %= 256;
+                }
+
+                sumOfSteps += stepValue;
+            }
+
+            return sumOfSteps;
+        }
+    }
+}
