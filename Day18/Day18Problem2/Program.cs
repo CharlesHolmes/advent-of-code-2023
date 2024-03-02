84ab7c38c2cd5b82088f244f93c33ffdcba3645d
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-02 13:44:27 -0600

    Test day 18 problem 2

--- Day18/Day18Problem2/Program.cs
+++ Day18/Day18Problem2/Program.cs
@@ -1,68 +1,12 @@
-﻿using System.Diagnostics;
-
-namespace Day18Problem2
+﻿namespace Day18Problem2
 {
-    public struct Coord
-    {
-        public long i;
-        public long j;
-    }
-
-    internal class Program
+    internal static class Program
     {
         static async Task Main(string[] args)
         {
             var inputLines = await File.ReadAllLinesAsync(args[0]);
-            var dugPoints = new List<Coord>();
-            var current = new Coord { i = 0, j = 0 };
-            long sum = 1; // starting point
-            dugPoints.Add(current);
-            foreach (string line in inputLines)
-            {
-                string hexInstructions = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[2];
-                string clean = string.Join("", hexInstructions.Where(c => char.IsAsciiLetterOrDigit(c)));
-                string key = "RDLU";
-                char direction = key[clean.Last() - '0'];
-                string hexDistanceString = clean.Substring(0, clean.Length - 1);
-                long hexDistance = long.Parse(hexDistanceString, System.Globalization.NumberStyles.HexNumber);
-                sum += hexDistance;
-                //Console.Out.WriteLine($"{clean} - {direction} - {hexDistance}");
-                int iDelta = 0, jDelta = 0;
-                switch (direction)
-                {
-                    case 'R':
-                        jDelta = 1;
-                        break;
-                    case 'D':
-                        iDelta = 1;
-                        break;
-                    case 'L':
-                        jDelta = -1;
-                        break;
-                    case 'U':
-                        iDelta = -1;
-                        break;
-                }
-
-                current = new Coord { i = current.i + (iDelta * hexDistance), j = current.j + (jDelta * hexDistance) };
-                dugPoints.Add(current);
-            }
-
-            // area of arbitrary polygon - https://en.wikipedia.org/wiki/Shoelace_formula
-            long interior = 0;
-            for (int i = 1; i <= dugPoints.Count; i++)
-            {
-                interior += dugPoints[i % dugPoints.Count].j * dugPoints[i - 1].i;
-            }
-
-            for (int i = 1; i <= dugPoints.Count; i++)
-            {
-                interior -= dugPoints[i % dugPoints.Count].i * dugPoints[i - 1].j;
-            }
-
-            sum += Math.Abs(interior);
-            Debug.Assert(sum % 2 == 1);
-            Console.Out.WriteLine(sum / 2 + 1);
+            var solver = new Solver();
+            Console.Out.WriteLine(solver.GetSolution(inputLines));
         }
     }
 }
---
+++ Day18/Day18Problem2/Solver.cs
@@ -0,0 +1,58 @@
+﻿namespace Day18Problem2
+{
+    public class Solver
+    {
+        public long GetSolution(string[] inputLines)
+        {
+            var dugPoints = new List<Coord>();
+            var current = new Coord { RowIndex = 0, ColumnIndex = 0 };
+            long sum = 1; // starting point
+            dugPoints.Add(current);
+            foreach (string line in inputLines)
+            {
+                string hexInstructions = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[2];
+                string clean = string.Join("", hexInstructions.Where(c => char.IsAsciiLetterOrDigit(c)));
+                string key = "RDLU";
+                char direction = key[clean.Last() - '0'];
+                string hexDistanceString = clean.Substring(0, clean.Length - 1);
+                long hexDistance = long.Parse(hexDistanceString, System.Globalization.NumberStyles.HexNumber);
+                sum += hexDistance;
+                int iDelta = 0, jDelta = 0;
+                switch (direction)
+                {
+                    case 'R':
+                        jDelta = 1;
+                        break;
+                    case 'D':
+                        iDelta = 1;
+                        break;
+                    case 'L':
+                        jDelta = -1;
+                        break;
+                    case 'U':
+                        iDelta = -1;
+                        break;
+                }
+
+                current = new Coord { RowIndex = current.RowIndex + (iDelta * hexDistance), ColumnIndex = current.ColumnIndex + (jDelta * hexDistance) };
+                dugPoints.Add(current);
+            }
+
+            // area of arbitrary polygon - https://en.wikipedia.org/wiki/Shoelace_formula
+            long interior = 0;
+            for (int i = 1; i <= dugPoints.Count; i++)
+            {
+                interior += dugPoints[i % dugPoints.Count].ColumnIndex * dugPoints[i - 1].RowIndex;
+            }
+
+            for (int i = 1; i <= dugPoints.Count; i++)
+            {
+                interior -= dugPoints[i % dugPoints.Count].RowIndex * dugPoints[i - 1].ColumnIndex;
+            }
+
+            sum += Math.Abs(interior);
+            long result = sum / 2 + 1;
+            return result;
+        }
+    }
+}
