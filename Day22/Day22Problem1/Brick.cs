905e6070d00dc9c829e766c40f2acd5338cc8db8
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-06 08:01:45 -0600

    Test day 22 problem 1

---
+++ Day22/Day22Problem1/Brick.cs
@@ -0,0 +1,22 @@
+﻿namespace Day22Problem1
+{
+    public class Brick
+    {
+        public Point Point1 { get; private init; }
+        public Point Point2 { get; private init; }
+
+        public Brick(Point p1, Point p2)
+        {
+            Point1 = p1;
+            Point2 = p2;
+        }
+
+        public bool IntersectsIfAtSameHeight(Brick other)
+        {
+            return Math.Min(Point1.X, Point2.X) <= Math.Max(other.Point1.X, other.Point2.X)
+                && Math.Max(Point1.X, Point2.X) >= Math.Min(other.Point1.X, other.Point2.X)
+                && Math.Max(Point1.Y, Point2.Y) >= Math.Min(other.Point1.Y, other.Point2.Y)
+                && Math.Min(Point1.Y, Point2.Y) <= Math.Max(other.Point1.Y, other.Point2.Y);
+        }
+    }
+}
