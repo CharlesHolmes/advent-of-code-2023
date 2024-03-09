4c6a63329b7b3d94d279c93fd07647ed328fc19c
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-09 10:02:10 -0600

    Test day 25

---
+++ Day25/Day25Problem1/Edge.cs
@@ -0,0 +1,13 @@
+﻿namespace Day25Problem1
+{
+    public class Edge
+    {
+        public Node Source { get; init; }
+        public Node Destination { get; init; }
+        public Edge TwinEdge { get; set; }
+        public override string ToString()
+        {
+            return $"{Source}->{Destination}";
+        }
+    }
+}
---
+++ Day25/Day25Problem1/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace Day25Problem1
+{
+    public class Node
+    {
+        public string Name { get; init; }
+        public List<Edge> Edges { get; } = new List<Edge>();
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
