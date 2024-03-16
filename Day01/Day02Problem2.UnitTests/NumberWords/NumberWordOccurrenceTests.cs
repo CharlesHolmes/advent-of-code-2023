46e15de498359953e99a7a43d63aad539cc5a930
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-16 12:40:55 -0500

    Add some tests

---
+++ Day01/Day02Problem2.UnitTests/NumberWords/NumberWordOccurrenceTests.cs
@@ -0,0 +1,23 @@
+﻿using Day01Problem2.NumberWords;
+
+namespace Day01Problem2.UnitTests.NumberWords
+{
+    [TestClass]
+    public class NumberWordOccurrenceTests
+    {
+        [TestMethod]
+        public void NumberWordOccurrence_WhenCreatedWithNumberWordAndIndex_ThenPropertiesAreSet()
+        {
+            // Arrange
+            var numberWord = NumberWord.AllNumberWords[0];
+            var index = 1;
+
+            // Act
+            var numberWordOccurrence = new NumberWordOccurrence(numberWord, index);
+
+            // Assert
+            Assert.AreEqual(numberWord, numberWordOccurrence.NumberWord);
+            Assert.AreEqual(index, numberWordOccurrence.Index);
+        }
+    }
+}
---
+++ Day01/Day02Problem2.UnitTests/NumberWords/NumberWordTests.cs
@@ -0,0 +1,34 @@
+﻿using Day01Problem2.NumberWords;
+using FluentAssertions;
+
+namespace Day01Problem2.UnitTests.NumberWords
+{
+    [TestClass]
+    public class NumberWordTests
+    {
+        [TestMethod]
+        public void NumberWord_ShouldBeCorrectType()
+        {
+            NumberWord.AllNumberWords.Should().AllBeOfType<NumberWord>();
+        }
+
+        [TestMethod]
+        public void NumberWord_AllNumberWords_ShouldContainOneThroughNine()
+        {
+            NumberWord.AllNumberWords.Select(w => new { w.Word, w.Value })
+                .Should()
+                .BeEquivalentTo(new[]
+                {
+                    new { Word = "one", Value = 1 },
+                    new { Word = "two", Value = 2 },
+                    new { Word = "three", Value = 3 },
+                    new { Word = "four", Value = 4 },
+                    new { Word = "five", Value = 5 },
+                    new { Word = "six", Value = 6 },
+                    new { Word = "seven", Value = 7 },
+                    new { Word = "eight", Value = 8 },
+                    new { Word = "nine", Value = 9 }
+                });
+        }
+    }
+}
