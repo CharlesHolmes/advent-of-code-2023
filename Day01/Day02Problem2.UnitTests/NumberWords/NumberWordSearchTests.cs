2097326ee25baf534cfdac64a952dcd92394fb8a
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-18 13:05:26 -0500

    Add tests (#89)

---
+++ Day01/Day02Problem2.UnitTests/NumberWords/NumberWordSearchTests.cs
@@ -0,0 +1,39 @@
+﻿using Day01Problem2.NumberWords;
+
+namespace Day01Problem2.UnitTests.NumberWords
+{
+    [TestClass]
+    public class NumberWordSearchTests
+    {
+        [TestMethod]
+        public void NumberWordSearch_ShouldReturnFirstWord()
+        {
+            var input = "12one432twoabc";
+            var search = new NumberWordSearch(input);
+            Assert.IsNotNull(search.FirstOccurrence);
+            Assert.AreEqual("one", search.FirstOccurrence.NumberWord.Word);           
+            Assert.AreEqual(1, search.FirstOccurrence.NumberWord.Value);
+            Assert.AreEqual(2, search.FirstOccurrence.Index);
+        }
+
+        [TestMethod]
+        public void NumberWordSearch_ShouldReturnLastWord()
+        {
+            var input = "12one432twoabc";
+            var search = new NumberWordSearch(input);
+            Assert.IsNotNull(search.LastOccurrence);
+            Assert.AreEqual("two", search.LastOccurrence.NumberWord.Word);
+            Assert.AreEqual(2, search.LastOccurrence.NumberWord.Value);
+            Assert.AreEqual(8, search.LastOccurrence.Index);
+        }
+
+        [TestMethod]
+        public void NumberWordSearch_ShouldReturnNullsIfNoWords()
+        {
+            var input = "12ond432twabc";
+            var search = new NumberWordSearch(input);
+            Assert.IsNull(search.FirstOccurrence);
+            Assert.IsNull(search.LastOccurrence);
+        }
+    }
+}
