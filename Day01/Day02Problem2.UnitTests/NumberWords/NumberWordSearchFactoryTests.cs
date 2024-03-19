a3b5bf2afb0386b309ec3941f451ab11ec38a36e
Charles Holmes <88447527+CharlesHolmes@users.noreply.github.com>
2024-03-19 06:00:15 -0500

    Add a couple tests

---
+++ Day01/Day02Problem2.UnitTests/NumberWords/NumberWordSearchFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using Day01Problem2.NumberWords;
+using FluentAssertions;
+
+namespace Day01Problem2.UnitTests.NumberWords
+{
+    [TestClass]
+    public class NumberWordSearchFactoryTests
+    {
+        [TestMethod]
+        public void NumberWordSearchFactory_ProvidesCorrectType()
+        {
+            var input = "12one432twoabc";
+            var search = new NumberWordSearchFactory().Create(input);
+            search.Should().BeAssignableTo<INumberWordSearch>();
+        }
+
+        [TestMethod]
+        public void NumberWordSearchFactory_ShouldInstantiateNumberWordSearch()
+        {
+            var input = "12one432twoabc";
+            var search = new NumberWordSearchFactory().Create(input);
+            search.Should().BeEquivalentTo(new NumberWordSearch(input));
+        }
+    }
+}
